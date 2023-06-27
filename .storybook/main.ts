0cad5c5e6741e34b67440e6c3e128bcf2ff785f1
Anton Kosar <anton.kosar@sobre.pl>
2023-06-27 16:35:42 +0200

    refactor: update storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,38 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+const path = require('path');
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        sass: {
-          // Require your Sass preprocessor here
-          implementation: require('sass'),
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          extensions: config.resolve.extensions,
+        }),
+      ];
+    }
+
+    if (config.module?.rules) {
+      config.module.rules.push({
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'],
+        include: path.join(__dirname, 'src/styles'),
+      });
+    }
+
+    return config;
   },
 };
 export default config;
